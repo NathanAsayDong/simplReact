56c6f8e29b9161125f47eb93b0e9f2f62ef8f1bc
Chase Nielsen <chasenielsen@Chases-MBP-5.app.byu.edu>
2024-02-26 15:27:33 -0700

    chase commit

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,35 @@
+// import { FC } from 'react';
+
+// interface NavBarProps {}
+
+// const NavBar: FC<NavBarProps> = () => (
+//    <>
+//       NavBar Component
+//    </>
+// );
+
+// export default NavBar;
 import { FC } from 'react';
-import './NavBar.scss';
 
 interface NavBarProps {}
 
 const NavBar: FC<NavBarProps> = () => (
-   <>
-      <div className='pages'>
-         <h2>Budgets</h2>
-         <h2>Upload CSV</h2>
-         <h2>Proforma</h2>
-         <h2 className='account'>Account</h2>
-      </div>
-   </>
+  <nav className="navbar">
+    <ul className="nav-menu">
+      <li className="nav-item">
+        <a href="#instant-balance">Instant Balance</a>
+      </li>
+      <li className="nav-item">
+        <a href="#future-balance">Future Balance</a>
+      </li>
+      <li className="nav-item">
+        <a href="#investments">Investments</a>
+      </li>
+      <li className="nav-item">
+        <a href="#expenses">Expenses</a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default NavBar;
