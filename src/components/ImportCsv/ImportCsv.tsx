4927fe7f7cbba6955a37aef4ad0a83c28fb37e00
NathanAsayDong <nathan.dong@enzy.co>
2024-02-27 19:33:27 -0700

    fixed spacing

--- src/components/ImportCsv/ImportCsv.tsx
+++ src/components/ImportCsv/ImportCsv.tsx
@@ -3,22 +3,22 @@ import { FC, useState } from 'react';
 interface ImportCsvProps {}
 
 const ImportCsv: FC<ImportCsvProps> = () => {
-   const [csvContent, setCsvContent] = useState<string>('');
- 
-   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-     const file = event.target.files?.[0];
-     if (file) {
-       const reader = new FileReader();
-       reader.onload = (e: ProgressEvent<FileReader>) => {
-         const text = e.target?.result;
-         setCsvContent(text as string);
-       };
-       reader.readAsText(file);
-     }
-   };
- 
-   return (
-     <>
+  const [csvContent, setCsvContent] = useState<string>('');
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        const text = e.target?.result;
+        setCsvContent(text as string);
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  return (
+    <>
       <div>
       <label htmlFor="upload-csv" style={{ backgroundColor: 'black', color: 'white', padding: '1.5px', borderRadius: '1px', cursor: 'pointer' }}>
         Upload File
@@ -26,8 +26,8 @@ const ImportCsv: FC<ImportCsvProps> = () => {
       <input type="file" id="upload-csv" accept=".csv" onChange={handleFileUpload} style={{ display: 'none' }} />
       {csvContent && <p>{csvContent}</p>}
     </div>
-     </>
-   );
- };
- 
- export default ImportCsv;
+    </>
+  );
+};
+
+export default ImportCsv;
