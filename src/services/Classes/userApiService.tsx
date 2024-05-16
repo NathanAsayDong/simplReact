bedf67ab60389913826580953f60e166eca6e322
NathanAsayDong <nathan.dong@enzy.co>
2024-05-16 17:27:58 -0600

    ran firebase init

---
+++ src/services/Classes/userApiService.tsx
@@ -0,0 +1,38 @@
+
+const baseUrl = __API_URL__;
+
+export const attemptLogin = async (email: string, password: string) => {
+    const response = await fetch(baseUrl + 'login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    });
+    if (!response.ok) {
+        return false;
+    }
+    const data = await response.json();
+    return data;
+}
+
+export const attemptCreateAccount = async (email: string, password: string) => {
+    const response = await fetch(baseUrl + 'createAccount', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    });
+    if (!response.ok) {
+        return false;
+    }
+    const data = await response.json();
+    return data;
+}
